bcd096a3abd959e3dfc7f1b7c27a0b9932330353
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2022-11-05 12:47:43 -0700

    Update src/index.ts: - Add a more specific type to the fileName parameter. - Modify the algorithm to take into account file extensions with more   than one dot.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import languageExtensions from "./assets/languageExtensions.json";
 
-export default function getProgrammingLanguage(fileName: string) {
-    let fileExtension: string | string[] = fileName.split(".");
-    fileExtension = fileExtension[fileExtension.length - 1];
-    return languageExtensions["." + fileExtension as keyof typeof languageExtensions];
+export default function getProgrammingLanguage(fileName: keyof typeof languageExtensions) {
+    // If there's no name, only a file extension.
+    
+    if (fileName.charAt(0) === ".") {
+        return languageExtensions[fileName as keyof typeof languageExtensions];
+    }
+
+    let fileExtensionArray: string[] = fileName.split(".");
+    let fileExtension;
+    if (fileExtensionArray.length > 1) {
+        // Remove the name of the file and leave only the extension, taking into account file extensions with more than one dot.
+        fileExtensionArray.shift();
+        fileExtension = fileExtensionArray.join(".");
+        fileExtension = "." + fileExtension;
+    } else {
+        fileExtension = "." + fileExtensionArray[0];
+    }
+    
+    return languageExtensions[fileExtension as keyof typeof languageExtensions];
 }
