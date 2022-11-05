697760317ee103a016e0f69a2a6601b4e20b284b
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2022-11-05 12:50:19 -0700

    Add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,20 @@ import getProgrammingLanguage from "../dist/index";
 
 it("Returns 'JavaScript'.", () => {
     expect(getProgrammingLanguage(".js")).toBe("JavaScript");
-})
+});
+
+it("Returns 'JavaScript'.", () => {
+    expect(getProgrammingLanguage("testfile.js")).toBe("JavaScript");
+});
+
+it("Returns JavaScript+ERB", () => {
+    expect(getProgrammingLanguage(".js.erb")).toBe("JavaScript+ERB");
+});
+
+it("Returns JavaScript+ERB", () => {
+    expect(getProgrammingLanguage("thisisatestfile.js.erb")).toBe("JavaScript+ERB");
+});
 
 it("Returns undefined.", () => {
     expect(getProgrammingLanguage(".fkldsjljfk")).toBeUndefined();
-})
+});
